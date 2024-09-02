9a746aaa67a7079affac3e57b6e0a438909b16d6
pedrohfc <pedrofreitas_ph@hotmail.com>
2024-09-02 15:48:48 -0300

    tec01

---
+++ tec01/index.js
@@ -0,0 +1,14 @@
+let indice = 13;
+let soma = 0;
+let k = 0;
+
+while (k < indice) {
+  k = k + 1;
+  soma = soma + k;
+}
+
+console.log(soma);
+
+// Ao final do processamento, qual sera o valor da variavel SOMA?
+// 91
+// O resultado e 91, que e a soma dos numeros inteiros de 1 ate 13 calculada pela formula da soma de uma  aritmetica
